251d79ff21b5950c4483395a0d23e86153514e31
João Bispo <joaobispo2077@gmail.com>
2021-07-31 12:35:45 -0300

    feat: add bottom icons menu in mobile

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -70,7 +70,23 @@ export const ProfileInfo = styled.div`
 `;
 
 export const BottomMenu = styled.div`
-  background: yellow;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  z-index: 2;
+
+  display: flex;
+  justify-content: space-between;
+
+  background: var(--primary);
+  width: 100%;
+  border-top: 1px solid var(--outline);
+
+  padding: 8px min(46px, max(10vw, 10px));
+
+  @media (min-width: 600px) {
+    display: none;
+  }
 `;
 
 const getBottomIconStyles = () => css`
